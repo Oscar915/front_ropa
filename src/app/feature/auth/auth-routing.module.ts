8492c9cf3107bd583cbac9a2d9200ec1b3c194fc
Oscar <oscarballestach@gmail.com>
2022-04-20 22:31:25 -0500

    Añadiendo rutas hijas para el home y login

--- src/app/feature/auth/auth-routing.module.ts
+++ src/app/feature/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  children:[
+    {
+      path:'login',
+      component: LoginComponent
+    },
+    {
+      path:'new',
+      component: RegisterComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
